ea742bf86aa74e3d3c040e6f8d8746ecc86bca7a
Osa <osaimola@gmail.com>
2020-07-30 13:33:31 -0600

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Git merging practice</h1>
-      <p>lets practice git </p>
-      
+      <h1>Git merging practice!</h1>
+
+      <h2>Some commands you can in git branches:</h2>
+      <ul>
+        <li>git checkout -b your-branch-name</li>
+        <li>git checkout your-branch-name</li>
+        <li>git merge your-other-branch-name</li>
+      </ul>
     </div>
   );
 }
