733bf31a25b3c91541460637327d441022c6f6c4
dimple sharma <dimplessharma@hotmail.com>
2020-07-30 10:44:48 -0600

    adding bransh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+  
 import React from 'react';
-
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>Git merging practice</h1>
-      <p>lets practice git </p>
-      
+      <h1>Git Branching / Merge Practice</h1>
+      <p>Let's practice Git Branching and Merging!</p>
+      <About />
     </div>
   );
 }
