2d6c4eec9c6ef923a2a430f0e14edfc4e680c172
dimple sharma <dimplessharma@hotmail.com>
2020-07-30 13:22:56 -0600

    practicing merging

---
+++ src/components/services.js
@@ -0,0 +1,23 @@
+import React from 'react';
+class Services extends React.Component
+{
+    render()
+    {
+        return(
+            <>
+
+            <h2>Services</h2>
+            <p>We offer services like</p>
+            <ul>
+                <li>web designing</li>
+                <li>web designing</li>
+                <li>web designing</li>
+            </ul>
+            </>
+
+        );
+
+    }
+   
+}
+export default Services;
